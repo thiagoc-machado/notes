afca33ab412dfab05d4a8923f8ae375696d493cb
thiagoc-machado <thiagocmachado@yahoo.com.br>
2023-01-05 16:36:29 +0100

    djngo parte done

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('notes/', views.getNotes, name="notes"),
+    path('notes/<str:pk>', views.getNote, name="note")
 ]
                              
                              
--- api/views.py
+++ api/views.py
@@ -1,8 +1,16 @@
+from django.http import response
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.serializers import Serializer
+from .models import Note
+from .serializers import NoteSerializer
+from api import serializers
+
 # Create your views here.
 
 
+@api_view(['GET'])
 def getRoutes(request):
     
     routes = [
@@ -38,4 +46,17 @@ def getRoutes(request):
         },
     ]
     
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getNote(request, pk):
+    notes = Note.objects.get(id=pk)
+    serializer = NoteSerializer(notes, many=False)
+    return Response(serializer.data)
